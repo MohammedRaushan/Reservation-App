439835d6268f5350192dea790498ac5fe1569fa3
Mohammed Raushan <113915370+MohammedRaushan@users.noreply.github.com>
2024-05-28 07:31:45 +0530

    Delete Reservation-Api directory

--- Reservation-Api/src/main/java/org/skylite/reservationapp/exception/NoBusPresentException.java
+++
@@ -1,8 +0,0 @@
-package org.skylite.reservationapp.exception;
-
-@SuppressWarnings("serial")
-public class NoBusPresentException extends RuntimeException{
-	public NoBusPresentException(String message) {
-		super(message);
-	}
-}
